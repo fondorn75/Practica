1f4e84b6e30cecc219edf61b8a53bc1c847d4df3
fondorn75 <zatoichi.cadzicu@gmail.com>
2021-11-16 22:34:48 +0300

    Add task 24 and 25

--- Examples004/Program.cs
+++ Examples004/Program.cs
@@ -12,23 +12,57 @@ int[] TabKvadrat(int n)
     return array;
 }
 
-int[] NewArray = TabKvadrat(12);
-
 string PrintArr(int[] array)
 {
     string res = string.Empty;
     for (int i = 0; i < array.Length; i++)
     {
-        res += $"{array[i]}, ";
+        res += $"{array[i]} ";
     }
     return res;
 }
 
+int[] NewArray = TabKvadrat(12);
+
 Console.WriteLine(PrintArr(NewArray));
 
 // 24. Найти кубы чисел от 1 до N
+
+int[] TabKub(int n)
+{
+    int result = 1;
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = result * result * result;
+        result++;
+    }
+    return array;
+}
+
+int[] NewKub = TabKub(8);
+
+Console.WriteLine(PrintArr(NewKub));
+
 // 25. Найти сумму чисел от 1 до А
+
+string Summa(int a)
+{
+    int b = 0;
+    string res = string.Empty;
+    for (int i = 1; i <= a; i++)
+    {
+        b = b + i;
+        res = res + b + " ";
+    }
+    return res;
+}
+Console.WriteLine(Summa(5));
+
 // 26. Возведите число А в натуральную степень B используя цикл
+
+
+
 // 27. Определить количество цифр в числе
 // 28. Подсчитать сумму цифр в числе
 // 29. Написать программу вычисления произведения чисел от 1 до N
