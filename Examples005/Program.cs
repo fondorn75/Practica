46f084aa0da026f56842dd75e8f414638813abba
Добрин Дмитрий <zatoichi.cadzicu@gmail.com>
2021-11-18 14:15:14 +0300

    add task 33 34

--- Examples005/Program.cs
+++ Examples005/Program.cs
@@ -27,8 +27,57 @@ Console.WriteLine(PrintArray(NewArray(8)));
 
 
 // 33. Задать массив из 12 элементов, заполненных числами из [0,9]. Найти сумму положительных/отрицательных элементов массива
+
+int[] NewArr(int n)
+{
+    int[] array = new int[n];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 10);
+    }
+
+    return array;
+}
+
+int[] arr = NewArr(12);
+
+Console.WriteLine(PrintArray(arr));
+
 // 34. Написать программу замену элементов массива на противоположные
+
+string Zamena(int[] array)
+{
+    string result = string.Empty;
+    int tmp = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        tmp = array[i] * -1;
+        result += tmp + " ";
+    }
+    return result;
+}
+
+Console.WriteLine(Zamena(arr));
+
+
 // 35. Определить, присутствует ли в заданном массиве, некоторое число 
+
+string ProverkaNum(int[] array, int n)
+{
+    string result = string.Empty;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == n) result = "Число" + n + " есть в массиве";
+        else result = "Числа " + n + " нет в массиве";
+    }
+
+    return result;
+}
+
+Console.WriteLine(ProverkaNum(arr, -6));
+
 // 36. Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
 // 37. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
 // 38. Найти сумму чисел одномерного массива стоящих на нечетной позиции
