c6e6c4df852f9a6d1509c7f709fffccbf235923f
Добрин Дмитрий <zatoichi.cadzicu@gmail.com>
2021-11-23 18:32:26 +0300

    add task 47

--- Examples006/Program.cs
+++ Examples006/Program.cs
@@ -148,3 +148,21 @@ Console.WriteLine();
 Scale(newarray, 2);
 
 //47. Написать программу копирования массива
+
+void CopyArray(int[,] array)
+{
+    int[,] array2 = new int[array.GetLength(0), array.GetLength(1)];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array2[i, j] = array[i, j];
+            Console.Write($"{array2[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Копия массива");
+CopyArray(newarray);
