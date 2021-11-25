9ed6cd24a77113bc26c9e0e378a2b2f32d2f9f64
Добрин Дмитрий <zatoichi.cadzicu@gmail.com>
2021-11-25 13:47:47 +0300

    add task 49 and 51

--- Examples007/Program.cs
+++ Examples007/Program.cs
@@ -34,15 +34,45 @@ Console.WriteLine(PrintArray(ArrayMN));
 
 // 49. Показать двумерный массив размером m×n заполненный вещественными числами
 
-
+int[,] ArrayMN2 = NewArray(4, 4, -10, 10);
+Console.WriteLine(PrintArray(ArrayMN2));
 
 // 50. В двумерном массиве n×k заменить четные элементы на противоположные
 
+int[,] ArrayNK(int[,] array)
+{
+    int[,] result = new int[array.GetLength(0), array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result[array.GetLength(0) - i - 1, array.GetLength(1) - j - 1] = array[i, j];
+        }
+    }
+    return result;
+}
 
+int[,] ArrayNK_New = ArrayNK(ArrayMN);
+Console.WriteLine(PrintArray(ArrayNK_New));
 
 // 51. Задать двумерный массив следующим правилом: Aₘₙ = m+n
 
+int[,] NewPravArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
 
+    return array;
+}
+
+int[,] ArrayNK_New2 = NewPravArray(5, 6);
+Console.WriteLine(PrintArray(ArrayNK_New2));
 
 // 52. В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
 
